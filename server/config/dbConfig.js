539461d342313b7eee2e9f21df1b72df5d666797
vivektangudu123 <vivektangudu2383@gmail.com>
2023-12-05 00:21:52 +0530

    remove -vvv

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -4,7 +4,11 @@ console.log(process.env.jwt_secret)
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    auth: {
+        user: process.env.MONGO_USERNAME, // Replace with your actual username
+        password: process.env.MONGO_PASSWORD, // Replace with your actual password
+    }
 });
 
 
