499240a123bcea4968d965ee17385cbd14ef2b30
vivektangudu123 <vivektangudu2383@gmail.com>
2023-12-04 23:32:39 +0530

    remove -vvv

--- server/server.js
+++ server/server.js
@@ -20,6 +20,12 @@ app.use(cors({
   methods: ['POST', 'GET', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use("/api/users", usersRoute);
 app.use("/api/books", booksRoute);
