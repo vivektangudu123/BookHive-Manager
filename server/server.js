55f5b419c84f3be2ea0a35ef401cd4c4ad7d783b
vivektangudu123 <vivektangudu2383@gmail.com>
2023-12-04 23:17:56 +0530

    remove -vvv

--- server/server.js
+++ server/server.js
@@ -1,43 +1,9 @@
-// const express = require("express");
-// const cors = require("cors");
-// const app = express();
-// app.use(express.json());
-// require("dotenv").config();
-// const dbConfig = require("./config/dbConfig");
-// const port = process.env.PORT || 5002;
-
-// const usersRoute = require("./routes/usersRoute");
-// const booksRoute = require("./routes/booksRoute");
-// const issuesRoute = require("./routes/issuesRoute");
-// const reportsRoute = require("./routes/reportsRoute");
-
-// app.use(cors({
-//   origin: '*',
-//   methods: ['POST', 'GET', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
-
-// app.use("/api/users", usersRoute);
-// app.use("/api/books", booksRoute);
-// app.use("/api/issues", issuesRoute);
-// app.use("/api/reports", reportsRoute);
-
-// const path = require("path");
-// __dirname = path.resolve();
-
-// if (process.env.NODE_ENV === "production") {
-//   // Set static folder
-//   app.use(express.static(path.join(__dirname, "/client/build")));
-
-//   // index.html for all page routes
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
-
-
-// app.listen(port, () => console.log(`Node server started at ${port}`));
 const express = require("express");
+const cors = require("cors");
+const corsOptions = {
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
 const app = express();
 app.use(express.json());
 require("dotenv").config();
@@ -48,6 +14,12 @@ const usersRoute = require("./routes/usersRoute");
 const booksRoute = require("./routes/booksRoute");
 const issuesRoute = require("./routes/issuesRoute");
 const reportsRoute = require("./routes/reportsRoute");
+
+app.use(cors({
+  origin: '*',
+  methods: ['POST', 'GET', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use("/api/users", usersRoute);
 app.use("/api/books", booksRoute);
@@ -66,5 +38,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
 
 app.listen(port, () => console.log(`Node server started at ${port}`));
