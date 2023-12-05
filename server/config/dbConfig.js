5ba7e13ee239841e5c29bef4c517151aa9b3ae32
vivektangudu123 <vivektangudu2383@gmail.com>
2023-12-05 20:09:18 +0530

    remove -vvv

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -17,5 +17,5 @@ connection.on('connected', () => {
 connection.on('error', (err) => {
     console.log('Mongo DB Connection Failed');
 })
-console.log(MONGO_URL);
+console.log(process.env.MONGO_URL);
 module.exports = connection;
