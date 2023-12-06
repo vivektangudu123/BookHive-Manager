f2eb15fc4382af95ce6b1f61a6fd7eeb3babd7bd
vivektangudu123 <vivektangudu2383@gmail.com>
2023-12-06 23:33:31 +0530

    remove -vvv

--- client/src/logger/logger.js
+++ client/src/logger/logger.js
@@ -1,36 +1,16 @@
-import winston from "winston";
-import { format } from "winston";
-const { combine, timestamp, json, printf } = format;
+import pino from 'pino';
+import { redact } from 'pino';
 
-const myFormat = printf(({ level, message, timestamp }) => {
-    return `[${timestamp}] [${level}] ${message}`;
-});
-
-const logger = winston.createLogger({
+const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
-    format: combine(
-        format.colorize(),
-        timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-        myFormat
-    ),
-    transports: [
-        new winston.transports.Console({
-            format: combine(
-                format.errors({ stack: true }),
-                format.simple()
-            ),
-        }),
-        new winston.transports.File({
-            filename: '../logs/server.log',
-            maxsize: 5242880,
-            maxFiles: 5,
-            tailable: true,
-            format: combine(
-                format.errors({ stack: true }),
-                format.json()
-            ),
-        }),
-    ],
+    prettyPrint: {
+        levelFirst: true,
+        translateTime: 'SYS:standard',
+    },
+    redact: {
+        paths: ['password', 'accessToken', 'refreshToken'],
+        censor: '***REDACTED***',
+    },
 });
 
 export default logger;
